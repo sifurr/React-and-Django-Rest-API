bb04ccd34da2f25162f634f3f6c999315ccfd250
Sifur Rahaman <saifurrshishir@gmail.com>
2021-01-09 00:28:44 +0600

    Serializers initiated

--- My_API/status/admin.py
+++ My_API/status/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from status.models import Status
 
 # Register your models here.
-admin.site.register(Status)
+
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ['user', 'content', 'image']
+
+    class Meta:
+        model = Status
+
+
+admin.site.register(Status, StatusAdmin)
--- My_API/status/views.py
+++ My_API/status/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+# Models
+from .models import Status
+
+# Serializer based on Status Model
+from .serializers import StatusSerializer
+
+# rest_framework view instead of django builtin view
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 
 # Create your views here.
+class StatusAPIView(APIView):
+    # builtin method
+    def get(self, request, format=None):
+        status_list = Status.objects.all()
+        status_serializer = StatusSerializer(status_list, many=True)
+        return Response(status_serializer.data)
