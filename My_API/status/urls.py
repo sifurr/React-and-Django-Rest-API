ba3b08e0f2a33fc8f5ebbbf0d6f7675e5623100e
Sifur Rahaman <saifurrshishir@gmail.com>
2021-01-11 13:27:49 +0600

    Testing CRUD request

--- My_API/status/urls.py
+++ My_API/status/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
-from .views import StatusAPIView, StatusListApiView, StatusCreateAPIView
+from .views import (
+    StatusAPIView,
+    StatusListApiView,
+    StatusCreateAPIView,
+    StatusDetailAPIView,
+    StatusUpdateAPIView,
+    StatusDeleteAPIView,
+)
 
 urlpatterns = [
     path('status/', StatusAPIView.as_view()),
     path('status/list/', StatusListApiView.as_view()),
     path('status/create/', StatusCreateAPIView.as_view()),
+    path('status/detail/<id>/', StatusDetailAPIView.as_view()),
+    path('status/update/<id>/', StatusUpdateAPIView.as_view()),
+    path('status/delete/<id>/', StatusDeleteAPIView.as_view()),
 ]
--- My_API/status/views.py
+++ My_API/status/views.py
@@ -20,11 +20,39 @@ class StatusAPIView(APIView):
         return Response(status_serializer.data)
 
 
+# to display all the posts
 class StatusListApiView(generics.ListAPIView):
     queryset = Status.objects.all()
     serializer_class = StatusSerializer
 
 
+# to create post
 class StatusCreateAPIView(generics.CreateAPIView):
     queryset = Status.objects.all()
     serializer_class = StatusSerializer
+
+
+# to display a single post detail
+class StatusDetailAPIView(generics.RetrieveAPIView):
+    queryset = Status.objects.all()
+    serializer_class = StatusSerializer
+    lookup_field = 'id'
+
+    # def get_object(self, *args, **kwargs):
+    #     kwargs = self.kwargs
+    #     kw_id = kwargs.get('id')
+    #     return Status.objects.get(id=kw_id)
+
+
+# to update a single post
+class StatusUpdateAPIView(generics.UpdateAPIView):
+    queryset = Status.objects.all()
+    serializer_class = StatusSerializer
+    lookup_field = 'id'
+
+
+# to delete a single post
+class StatusDeleteAPIView(generics.DestroyAPIView):
+    queryset = Status.objects.all()
+    serializer_class = StatusSerializer
+    lookup_field = 'id'
